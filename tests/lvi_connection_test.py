9c9495c0cf7cf581cb66fa791fe48efb4428b4bd
Helge Waastad <helge.waastad@gmail.com>
2019-12-17 14:15:29 +0100

    initial test add

---
+++ tests/lvi_connection_test.py
@@ -0,0 +1,15 @@
+import nose, sys, logging
+from unittest import TestCase
+
+from lvi import Lvi
+
+class TestConnection(TestCase):
+
+    def test_constructor(self):
+        lvi_connection = Lvi('asdads','asdasd)
+        lvi_connection.sync_connect()
+       # lvi_connection.close_connection()
+
+
+if __name__ == '__main__':
+    nose.run()
